824bacd61d05117b004460fd7153e40f4231c44b
saurabh jain <luckyjain0107@gmai.com>
2022-07-21 11:24:07 +0530

    Count the Character of string

---
+++ CountCharacter.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class CountCharacter {
+    public static void main(String[] args) {
+        String string = "Count the total number Character";
+      int totalcount= string.length();
+       int count =totalcount;
+
+        System.out.println("total count charcater is:" +count);
+        }
+
+        }
